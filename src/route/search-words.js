5a631864fb5571018affb4948c7f10e88e386ee5
Dmitry Domanski <dmitry.domanski@gmail.com>
2018-11-18 01:44:47 +0200

    top 10 requests fetched

--- src/route/search-words.js
+++ src/route/search-words.js
@@ -10,7 +10,7 @@ const searchRouter = module.exports = new Router();
 searchRouter.post('/search', (req, res) => {
 
     const entry = new SearchEntry();
-    const query = { searchWord: req.query.term };
+    const query = { searchWord: req.query.term.toLowerCase() };
     const update = { $inc: { numEntries: 1 } }
 
     SearchEntry.findOneAndUpdate(query, update)
@@ -35,7 +35,7 @@ searchRouter.post('/search', (req, res) => {
 const api = process.env.ITUNES_API || 'https://itunes.apple.com/search?term=';
 
 const getContent = (term) => {
-    const appendix = '&media=musicVideo'; 
+    const appendix = '&media=musicVideo';
     return fetch(`${api}${term}${appendix}`);
 }
 
@@ -45,3 +45,14 @@ searchRouter.get('/search', (req, res) => {
         .then(responseData => res.send(responseData.results))
         .catch(err => res.status(500).json({ error: err }));
 });
+
+searchRouter.get('/top', (req, res) => {
+    SearchEntry
+        .find()
+        .sort({ numEntries: -1 })
+        .limit(10)
+        .then(result => {
+            const queries = result.map(query => query.searchWord);
+            res.send(queries);
+        });
+})
